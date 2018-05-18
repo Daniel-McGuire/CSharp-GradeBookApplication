a5ad460a9f474b08dbf3f07fb079ee5d87c7da99
Daniel McGuire <Daniel-McGuire@users.noreply.github.com>
2018-05-18 15:17:53 +0100

    commit

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -29,6 +29,15 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+             if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a students overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
 
     }
 }
